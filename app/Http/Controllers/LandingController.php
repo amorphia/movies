f3f80b6d3ac0750a39923ae77417887b99ef4c4a
Jeremy Kalgreen <jeremy@jeremykalgreen.com>
2020-05-22 02:26:47 -0400

    Add guest middelwear to landing

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class LandingController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
     public function index(){
         return view( 'welcome' );
     }
